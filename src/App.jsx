9fde498df82030be130cd2855d18d84c418ba200
Khaledgaafar <khaledgafaar@gmail.com>
2025-03-29 01:43:57 +0200

    enhance the movie-detail and create tv-detail with all routes and api

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import MoviesList from './pages/MoviesList';
 import MovieDetail from './pages/MovieDetail';
 import TVShows from './pages/TVShows';
-import TVShowDetail from './pages/TVShowDetail';
+import TvDetail from './pages/TVShowDetail';
 import Wishlist from './pages/Wishlist';
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path="/movies" element={<MoviesList />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/tv-shows" element={<TVShows />} />
-        <Route path="/tv/:id" element={<TVShowDetail />} />
+        <Route path="/tv/:id" element={<TvDetail />} />
         <Route path="/about" element={<div className="container mt-5 pt-5"><h1>About</h1></div>} />
         <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
